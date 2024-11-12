f53015506bda44bee9c4bb3ad0dcc9d1395e8664
jannyyyy1 <jannyzzz@naver.com>
2024-11-12 15:27:41 +0900

    파티클 안에 있던 숨은 중력 빼고, applyforce적용

--- Particle.js
+++ Particle.js
@@ -3,20 +3,26 @@
 // A simple Particle class
 class Particle{
     constructor(position){
-        this.acceleration = createVector(0, 0.05);
-    this.velocity = createVector(random(-1, 1), random(-1, 0));
-    this.position = position.copy();
-    this.lifespan = 255;
+        this.acceleration = createVector(0, 0.5);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
     }
     run(){
         this.update();
         this.display();
     }
 
+    applyForce(force){
+        this.acceleration.add(force);
+    }
+
     update(){
         this.velocity.add(this.acceleration);
         this.position.add(this.velocity);
         this.lifespan -= 2;
+
+        this.acceleration.set(0,0);
     }
 
     display(){    
