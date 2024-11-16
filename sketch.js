1cdc7c63c8158aace67fec170c98d0d5b75afa2a
jannyyyy1 <jannyzzz@naver.com>
2024-11-16 18:03:46 +0900

    마우스 클릭 함수 수정
    클릭 시에 단일 raindrop 만 생성하게 수정, 기존 rain 시스템 초기화 후 방출기 생성까지.

--- sketch.js
+++ sketch.js
@@ -23,12 +23,14 @@ function draw() {
 
 }
 
-function mouseClicked(){
-   let mPos = createVector(mouseX,mouseY)
-   let system = new Rain(mPos);
-   systems.push(system);
-
+function mouseClicked() {
+  let mPos = createVector(mouseX, mouseY);
+  let drop = new Raindrop(mPos); 
+  systems = []; 
+  systems.push(new Rain(mPos)); // 새로운 방출기 생성
+  systems[0].addRaindrop(); // 해당 방출기에서 처음 물방울 추가
 }
+
 
 
 
